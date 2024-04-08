39f912c4a2ce7242a6df968c5e570604e25d3b81
Kálóczi Kornél <kkornel.ctr@tenable.com>
2024-04-08 15:34:37 +0200

    try out url

--- backend/api/index.js
+++ backend/api/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
-app.use(cors());
+app.use(cors({
+    origin: 'https://backend-phi-three-73.vercel.app/'
+}));
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
 app.listen(5000, () => console.log("Server ready on port 5000."));
